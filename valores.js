cd62450f604c3150d40b797a28579c85173bad6a
Gacho <lcaceres392@gmail.com>
2022-05-24 10:35:40 -0500

    Realizacion Hasta La clase 8 del curso basico de java script

---
+++ valores.js
@@ -0,0 +1,12 @@
+40
+
+"Gabriel Caceres"
+
+true
+false
+
+null
+undefined
+
+[1,2,3]
+{nombre : "Gabriel"}
---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Luis";
+
+//declarar
+var edad;
+
+edad
+
+//inicializar
+
+edad = 30;
+
+
+var elementos = ["Compu", "Celular"]
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+persona;
